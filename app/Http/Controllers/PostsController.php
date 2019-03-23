664ba4aefcbd8b80d2c4768bbabca74ac80cd4d2
Alex Leye <anley1@student.monash.edu>
2019-03-23 15:28:02 +1100

    add blog post Service layer

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,10 +5,10 @@ namespace LaraTest\Http\Controllers;
 use LaraTest\Http\Requests\BlogPostRequests;
 use LaraTest\Post;
 use Illuminate\Support\Facades\Storage;
+use LaraTest\Services\BlogService;
 
 class PostsController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index','show']]);
@@ -52,34 +52,8 @@ class PostsController extends Controller
         $validated = $request->validated();
 
         // Handle a file upload
-        if($request->hasFile('cover_image'))
-        {
-            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
-
-            //Get just extension
-            $extension = $request->file('cover_image')->getClientOriginalExtension();
-
-            // Store filename
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-
-            // Upload image
-            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
-
-        }
-        else
-        {
-            $fileNameToStore = 'no_image.jpg';
-        }
-
-        // Create post
-        $post = new Post;
-        $post->title = $request->input('title');
-        $post->body = $request->input('body');
-        $post->user_id = auth()->user()->id;
-        $post->cover_image = $fileNameToStore;
-        $post->save();
+        $blogService = new BlogService();
+        $blogService->savePost($request);
 
         // Go back to index page
         return redirect('/posts')->with('success','Post Created');
@@ -136,32 +110,8 @@ class PostsController extends Controller
         $validated = $request->validated();
             
         // Handle a file upload
-        if($request->hasFile('cover_image'))
-        {
-            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
-
-            //Get just extension
-            $extension = $request->file('cover_image')->getClientOriginalExtension();
-
-            // Store filename
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-
-            // Upload image
-            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
-
-        }
-
-        // Create post
-        $post = Post::find($id);
-        $post->title = $request->input('title');
-        $post->body = $request->input('body');
-        $post->user_id = auth()->user()->id;
-        if($request->hasFile('cover_image')){
-            $post->cover_image = $fileNameToStore; // only change if the user changes it
-        }
-        $post->save();
+        $blogService = new BlogService();
+        $blogService->updatePost($request,$id);
 
         // Go back to index page
         return redirect('/posts')->with('success','Post Updated');
---
+++ app/Services/BlogService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace LaraTest\Services;
+
+use LaraTest\Http\Requests\BlogPostRequests;
+use LaraTest\Post;
+use Illuminate\Support\Facades\Storage;
+
+class BlogService
+{
+
+    public function savePost(BlogPostRequests $request)
+    {
+        if($request->hasFile('cover_image'))
+        {
+            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // Get just filename
+            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+            //Get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+
+            // Store filename
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+
+            // Upload image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+
+        }
+        else
+        {
+            $fileNameToStore = 'no_image.jpg';
+        }
+
+        // Create post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->user_id = auth()->user()->id;
+        $post->cover_image = $fileNameToStore;
+        $post->save();
+
+    }
+
+    public function updatePost(BlogPostRequests $request,$id)
+    {
+        if($request->hasFile('cover_image'))
+        {
+            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // Get just filename
+            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+            //Get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+
+            // Store filename
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+
+            // Upload image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+
+        }
+
+        // Create post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->user_id = auth()->user()->id;
+        if($request->hasFile('cover_image')){
+            $post->cover_image = $fileNameToStore; // only change if the user changes it
+        }
+        $post->save();
+
+    }
+}
